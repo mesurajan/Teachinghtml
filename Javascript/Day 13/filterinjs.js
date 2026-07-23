8571ac9b45107e34b123d163bdc92d5f42e3bfe8
mesurajan <surajansth1995@gmail.com>
2026-07-23 14:18:00 +0545

    learned about DOM

---
+++ Javascript/Day 13/filterinjs.js
@@ -0,0 +1,75 @@
+// const prices = [
+//   1500, 2000, 2500, 3000, 3500, 4000, 10, 40, 4982, 5000, 6000, 7000, 8000,
+//   9000, 10000,
+// ];
+
+// const filteredPrices = prices.filter((price) => {
+//   return price > 1000;
+// });
+
+// console.log(filteredPrices);
+
+// const products = [
+//   {
+//     id: 1,
+//     name: "T-shirt",
+//     actualprice: 1000,
+//     discountedprice: 800,
+//     instock: true,
+//     color: ["red", "blue", "green"],
+//     description: "A comfortable cotton T-shirt for casual wear.",
+//     category: "MEN",
+//   },
+//   {
+//     id: 2,
+//     name: "Jeans",
+//     actualprice: 2000,
+//     discountedprice: 1600,
+//     instock: false,
+//     color: ["black", "blue"],
+//     description: "Durable denim jeans for everyday wear.",
+//     category: "MEN",
+//   },
+//   {
+//     id: 3,
+//     name: "Kurthi",
+//     actualprice: 3000,
+//     discountedprice: 2500,
+//     instock: true,
+//     color: ["red", "blue", "green"],
+//     description: "A comfortable cotton Kurthi for casual wear.",
+//     category: "WOMEN",
+//   },
+// ];
+
+// const filteredProducts = products.filter((i) => {
+//   return i.category === "MEN";
+// });
+
+// filteredProducts.forEach((products) => {
+//   console.log("Product ID :", products.id);
+//   console.log("Product Name :", products.name);
+//   console.log("Actual Price :", products.actualprice);
+//   console.log("Discounted Price :", products.discountedprice);
+//   console.log("In Stock :", products.instock);
+//   console.log("Color :", products.color);
+//   console.log("Description :", products.description);
+//   console.log("-----------------------------");
+// });
+// console.log(filteredProducts);
+
+//create an array name ListofFobes and list there 5 fobes with 6 properties of each fobes
+// like name ,age, networth, company, country, description and filter only those with
+// networth greater than 1000000 and display the details of each fobes using map
+
+//create an array name ListofCountries and list there 5 countries with 6 properties of each country
+// like name ,capital, population, area, currency, description and filter only those with
+// population greater than 1000000 and display the details of each country using map
+
+//create an array name ListofMovies and list there 5 movies with 6 properties of each movie
+// like title ,director, release year, genre, rating, description and filter only those with
+// rating greater than 8 and display the details of each movie using map
+
+//create an array name ListofCars and list there 5 cars with 6 properties of each car
+// like make ,model, year, price, color, description and filter only those with
+// price greater than 50000 and display the details of each car using map
---
+++ Javascript/Day 14/DOM.js
@@ -0,0 +1,12 @@
+const demo1 = document.getElementById("demo");
+demo1.innerHTML = "Hello <b>World</b>";
+
+function handlebuynow() {
+  console.log("Buy Now button clicked");
+}
+handlebuynow();
+
+function handledelete() {
+  console.log("Are you sure you want to delete this item?");
+}
+handledelete();
